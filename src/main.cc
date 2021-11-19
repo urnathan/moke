f1d7c11f8e59fccb8acfb929ac8a32ebed069a4e
Nathan Sidwell <nathan@acm.org>
2021-11-19 11:07:36 -0500

    search for keyboard

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,11 @@
 // License: Affero GPL v3.0
 
 // Although this is C++, we're only using it for syntax and staying in
-// the C subset of semantics.
+// the C subset of runtime.
 
 #include "wakamecfg.h"
 // C
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -19,9 +20,18 @@
 
 namespace 
 {
-auto const &UinputDev = "/dev/uinput";
-auto const &InputDevDir = "/dev/input";
-auto const &KeyboardName = "AT Translated Set 2 keyboard";
+#if __CHAR_BIT__
+unsigned const charBits = __CHAR_BIT__;
+#else
+unsigned const charBits = 8; // 'cos it is, isn't it
+#endif  
+
+using ul_t = unsigned long;
+auto const ulBits = sizeof (ul_t) * charBits;
+
+auto const &uinputDev = "/dev/uinput";
+auto const &inputDevDir = "/dev/input";
+auto const &keyboardName = " keyboard$";
 
 // FIXME: think about how to make this more flexible
 unsigned const mouseKey = KEY_LEFTMETA;
@@ -31,99 +41,148 @@ unsigned const mouseRightAlt = KEY_RIGHTALT;
 
 namespace
 {
-bool verbose = false;
+char const *progName = "wakame";
+bool flagVerbose = false;
 }
 
 namespace 
 {
-bool TestBit (unsigned long const *bits, unsigned n)
-{
-  return (bits[n / (sizeof (unsigned long) * 8)]
-	  >> (n & ((sizeof (unsigned long) * 8) - 1))) & 1;
+void Inform (char const *fmt, ...) {
+  va_list args;
+  fprintf (stderr, "%s:", progName);
+  va_start (args, fmt);
+  vfprintf (stderr, fmt, args);
+  va_end (args);
+  fprintf (stderr, "\n");
 }
 
-int FindKeyboard ()
+#define Verbose(fmt, ...)						\
+  (flagVerbose ? Inform (fmt __VA_OPT__(, __VA_ARGS__)) : void(0))
+
+template<typename T>
+constexpr bool TestBit (T const *bits, unsigned n)
+{
+  return (bits[n / sizeof (T) * charBits] >> (n & (sizeof (T) * charBits - 1)))
+    & 1;
+}
+
+// See if FD is the keyboard we want.  Must match wanted and accept
+// key events.
+bool IsKeyboard (int fd, char const *fName, char const *wantedName = nullptr)
+{
+  int version;
+  char devName[256];
+  int sDevLen;
+  unsigned long typebits;
+
+  if (ioctl (fd, EVIOCGVERSION, &version) < 0
+      || (sDevLen = ioctl (fd, EVIOCGNAME (sizeof (devName)), devName)) < 0
+      || ioctl (fd, EVIOCGBIT (0, EV_CNT), &typebits) < 0)
+    {
+      Verbose ("rejecting `%s': not an EVIO device", fName);
+      return false;
+    }
+
+  // The name length includes the trailing NUL, check it
+  unsigned devLen = unsigned (sDevLen);
+  if (devLen > sizeof (devName) || !devLen || devName[devLen-1])
+    {
+      Verbose ("rejecting `%s': name badly formed", fName);
+      return false;
+    }
+  devLen--; // Make it the usual len we care about
+
+  if (!(typebits & (1 << EV_KEY)))
+    {
+      Verbose ("rejecting `%s' (%s): not a keyboard", fName, devName);
+      return false;
+    }
+
+  if (wantedName && wantedName[0])
+    {
+      auto wantedLen = strlen (wantedName);
+      bool anchorStart = wantedName[0] == '^';
+      bool anchorEnd = wantedName[wantedLen - 1] == '$';
+      bool matched = false;
+      unsigned matchLen = wantedLen - anchorStart - anchorEnd;
+
+      if (matchLen > devLen)
+	; // name too long
+      else if (anchorStart || anchorEnd)
+	{
+	  if (anchorStart && anchorEnd && matchLen != devLen)
+	    ; // want exact match and lengths differ
+	  else
+	    matched = !memcmp (&devName[anchorStart ? 0 : devLen - matchLen],
+			       &wantedName[anchorStart], matchLen);
+	}
+      else
+	matched = strstr (devName, wantedName);
+
+      if (!matched)
+	{
+	  Verbose ("rejecting `%s' (%s): does not match `%s'",
+		   fName, devName, wantedName);
+	  return false;
+	}
+    }
+
+  Verbose ("found keyboard `%s' (%s)", fName, devName);
+
+  return true;
+}
+
+// Find and open the keyboard, return an fd or -1 on failure.
+// @parm(wanted) either filename in input dir, or name fragment.
+// Fragment can be anchored at start with ^ or end with $, but it is
+// not a regexp.
+int FindKeyboard (char const *wanted = nullptr)
 {
   int fd = -1;
-  int dirfd = open (InputDevDir, O_RDONLY | O_DIRECTORY);
-  if (DIR *dir = fdopendir (dirfd))
+  int dirfd = open (inputDevDir, O_RDONLY | O_DIRECTORY);
+
+  if (dirfd < 0)
+    Inform ("cannot open %s: %m", inputDevDir);
+  else
     {
-      while (struct dirent const *ent = readdir (dir))
-	if (ent->d_type == DT_CHR)
-	  {
-	    fd = openat (dirfd, ent->d_name, O_RDONLY | O_NONBLOCK, 0);
-	    if (fd >= 0)
+      // Try a direct open of something that might be file-like --
+      // begins with '/' or './' or doesn't contain ' '
+      if (wanted &&
+	  (wanted[wanted[0] == '.'] == '/' || !strchr (wanted, ' ')))
+	{
+	  fd = openat (dirfd, wanted, O_RDONLY | O_NONBLOCK, 0);
+	  if (fd >= 0)
+	    {
+	      if (IsKeyboard (fd, wanted, nullptr))
+		{
+		  close (dirfd);
+		  return fd;
+		}
+	      close (fd);
+	      fd = -1;
+	    }
+	}
+
+      if (DIR *dir = fdopendir (dirfd))
+	{
+	  while (struct dirent const *ent = readdir (dir))
+	    if (ent->d_type == DT_CHR)
 	      {
-		// Interrogate
-		printf ("file:%s", ent->d_name);
-		char name[256];
-		ioctl (fd, EVIOCGNAME (sizeof (name)), name);
-		printf (" name:%s", name);
-		printf ("\n  ");
-
-		int version;
-		if (ioctl (fd, EVIOCGVERSION, &version) < 0)
-		  printf ("unknown");
-		else
+		fd = openat (dirfd, ent->d_name, O_RDONLY | O_NONBLOCK, 0);
+		if (fd >= 0)
 		  {
-		    printf (" version:%u.%u.%u", version >> 16,
-			    (version >> 8) & 0xff, version & 0xff);
-
-		    unsigned short id[4];
-		    ioctl (fd, EVIOCGID, id);
-		    printf (" ID: bus=0x%x, vendor=0x%x, product=0x%x,"
-			    " version=0x%x", id[ID_BUS], id[ID_VENDOR],
-			    id[ID_PRODUCT], id[ID_VERSION]);
-		    printf ("\n  ");
-
-		    ioctl (fd, EVIOCGPHYS (sizeof (name)), name);
-		    printf (" phys:%s", name);
-		    printf ("\n  ");
-
-		    printf ("props: ");
-		    unsigned long propbits;
-		    ioctl (fd, EVIOCGPROP (INPUT_PROP_CNT), &propbits);
-		    for (unsigned ix = 0; ix != INPUT_PROP_CNT; ix++)
-		      {
-			if (!(ix & 7) && ix)
-			  printf (",");
-			printf ("%d", unsigned (propbits >> ix) & 1);
-		      }
-		    printf ("\n  ");
-
-		    printf ("types: ");
-		    unsigned long typebits;
-		    ioctl (fd, EVIOCGBIT (0, EV_CNT), &typebits);
-		    for (unsigned type = 0; type != EV_CNT; type++)
-		      {
-			if (!(type & 7) && type)
-			  printf (",");
-			printf ("%d", unsigned (typebits >> type) & 1);
-		      }
-		    if (typebits & (1 << EV_KEY))
-		      {
-			printf ("\n ");
-			unsigned long keybits[KEY_CNT
-					      / sizeof (unsigned long) / 8];
-			ioctl (fd, EVIOCGBIT (EV_KEY, KEY_CNT), keybits);
-			if (TestBit (keybits, mouseKey))
-			  printf (" mouseKey");
-			if (TestBit (keybits, mouseMiddleAlt))
-			  printf (" mouseMiddleAlt");
-			if (TestBit (keybits, mouseMiddleAlt))
-			  printf (" mouseMiddleAlt");
-		      }
+		    if (IsKeyboard (fd, ent->d_name, wanted))
+		      break;
+		    close (fd);
+		    fd = -1;
 		  }
-		printf ("\n");
-
-		close (fd);
-		fd = -1;
 	      }
-	  }
-      closedir (dir);
+	  closedir (dir);
+	}
+      else
+	close (dirfd);
     }
-  else if (dirfd >= 0)
-    close (dirfd);
+
   return fd;
 }
 
@@ -132,7 +191,10 @@ int FindKeyboard ()
 
 int main (int, char *[])
 {
-  int keyFd = FindKeyboard ();
+  // FIXME: set progName
+  // FIXME: read options
+  flagVerbose = true;
+  int keyFd = FindKeyboard (keyboardName);
   if (keyFd < 0)
     return 1;
   
